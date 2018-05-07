357d47d53da5f5cc4821eb83be6597573b4722b4
Alan Chen <alanchen1218@gmail.com>
2018-05-07 15:33:42 -0700

    Create an app with a UITableView with 6 rows, each with a different background color of the rainbow.

--- RainbowRoad/ViewController.swift
+++ RainbowRoad/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var colors = [UIColor.red,  UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        self.tableView.rowHeight = 120.0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
+
 
